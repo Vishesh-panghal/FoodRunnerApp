c04feeeb18b4bfe7240e6ae90efab938dc34146f
Vishesh Panghal <visheshpanghal12@gmail.com>
2022-06-01 22:56:05 +0530

    remove password editText form forgotPass..! & remove all errors

--- app/src/main/java/com/vishesh/foodrunner/ForgotPassword.kt
+++ app/src/main/java/com/vishesh/foodrunner/ForgotPassword.kt
@@ -1,11 +1,19 @@
 package com.vishesh.foodrunner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ForgotPassword : AppCompatActivity() {
+  private  lateinit var logIn : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
+        logIn = findViewById(R.id.LogIn)
+        logIn.setOnClickListener {
+            val intent = Intent(this@ForgotPassword, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/vishesh/foodrunner/Register.kt
+++ app/src/main/java/com/vishesh/foodrunner/Register.kt
@@ -1,11 +1,19 @@
 package com.vishesh.foodrunner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class Register : AppCompatActivity() {
+    private lateinit var login: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        login = findViewById(R.id.LogIn2)
+        login.setOnClickListener {
+            val intent = Intent(this@Register, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
