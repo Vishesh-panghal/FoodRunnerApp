a02915b700ee2609a03988a60d2193b080edd669
Vishesh Panghal <visheshpanghal12@gmail.com>
2022-06-01 16:42:19 +0530

    making forgot password activity & remove all errors..!

--- app/src/main/java/com/vishesh/foodrunner/MainActivity.kt
+++ app/src/main/java/com/vishesh/foodrunner/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.vishesh.foodrunner
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var txtForgotPassword: TextView
+    private lateinit var register: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val frgtpass: TextView = findViewById(R.id.frgtpass)
-        val register: TextView = findViewById(R.id.register)
-        frgtpass.movementMethod = LinkMovementMethod.getInstance()
-        register.movementMethod = LinkMovementMethod.getInstance()
+        txtForgotPassword = findViewById(R.id.frgtpass)
+        register = findViewById(R.id.register)
+        txtForgotPassword.setOnClickListener {
+            val intent = Intent(this@MainActivity, ForgotPassword::class.java)
+            startActivity(intent)
+        }
+        register.setOnClickListener {
+            val intent = Intent(this@MainActivity, Register::class.java)
+            startActivity(intent)
+        }
     }
 }
